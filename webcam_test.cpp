6524648d3e71365ea1d58e539f20390b023200ee
Brian Hernandez <Brian@Hernandez-cs.com>
2017-12-04 17:26:09 -0500

    Add sample code for canny edge detection, drawing ball, and editing pixels as comments

--- webcam_test.cpp
+++ webcam_test.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
+
 using namespace cv;
 using namespace std;
 
@@ -11,15 +12,40 @@ int main() {
     cout << "cannot open camera";
   }
 
+  Mat edges;
+  namedWindow("edges", 1);
+  int x = 0;
+  int y = 0;
   while (true) {
     Mat cameraFrame;
     stream1.read(cameraFrame);  // read webcam frame
-    cout << "M = "<< endl << " "  << cameraFrame.rows << ", "
-         << cameraFrame.cols << endl << endl;  //  image size
-    cout << cameraFrame.at<Vec3b>(Point(30,30)) << endl;  // RGB at x,y = 30,30
-    imshow("cam", cameraFrame);  // draw camera frame
-    if (waitKey(30) >= 0)  // wait for user input for 30 milliseconds and quit if received
-      break;
+
+    // cout << "M = "<< endl << " "  << cameraFrame.rows << ", "
+    //      << cameraFrame.cols << endl << endl;  //  image size
+    // cout << cameraFrame.at<Vec3b>(Point(30,30)) << endl;  // RGB at x,y = 30,30
+
+    //CODE TO CANNY EDGE DETECTOR
+    // cvtColor(cameraFrame, edges, CV_BGR2GRAY);
+    // GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+    // Canny(edges, edges, 0, 100, 3);
+
+    //CODE TO ACCESS PIXELS
+    // uchar r, g, b;
+    // for(int i = 0; i < cameraFrame.rows; ++i){
+    //   Vec3b* pixel = cameraFrame.ptr<Vec3b>(i);
+    //   for(int j = 0; j < cameraFrame.cols; ++j){
+    //     pixel[j][0] = pixel[j][2];
+    //     pixel[j][2] = pixel[j][0];
+    //   }
+    // }
+
+    //CODE TO DRAW A CIRCLE
+    // circle(cameraFrame, Point(x,y), 100, Scalar(255, 255, 255), -1);
+    // x+=20;
+    // y+=20;
+
+    imshow("cameraFrame", cameraFrame);
+    if(waitKey(30) >= 0) break;
   }
   return 0;
 }
