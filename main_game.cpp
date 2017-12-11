d46fc98b5b310f872b4cd504f26c21063d40ece7
Carol Chau <carol.chau@macaulay.cuny.edu>
2017-12-10 21:26:36 -0500

    start ball at center of image fix minor issues

--- main_game.cpp
+++ main_game.cpp
@@ -38,68 +38,67 @@ int main(int argc, char** argv) {
     }
   }
 
-  // Create ball at position 0,0
-  int radius = 25;  // 50px ball radius
+  // Create ball at the center of image
+  int radius = 25;
   Vec2f velocity{0,0};  // initial ball velocity
   Scalar color{255,255,255};  // white ball color
-  Ball ball{width, height, radius, 100, 100, velocity, color};
+  Ball ball{width, height, radius, width/2, height/2, velocity, color};
 
   int left_score = 0; //int to keep track of left score
   int right_score = 0; //int to keep track of right score
 
-  Mat mirrorFrame;
-  Mat cameraFrame;
+
   while (true) {
-    cam_stream.read(mirrorFrame);  // read webcam frame
-    flip(mirrorFrame, cameraFrame, 1);
+    cam_stream.read(frame);  // read webcam frame
+    flip(frame, frame, 1);
     Mat fgmask = getForegroundMask(background, frame);
 
-    cvtColor(cameraFrame, grey_frame, CV_BGR2GRAY); // convert to grayscale
-    GaussianBlur(grey_frame, grey_frame, Size(21,21), 0); // remove noise   
-    if(previous.empty()) grey_frame.copyTo(previous);
-    absdiff(previous, grey_frame, difference);
-    threshold(difference, difference, threshold_value, 255, THRESH_BINARY);
+    // cvtColor(frame, grey_frame, CV_BGR2GRAY); // convert to grayscale
+    // GaussianBlur(grey_frame, grey_frame, Size(21,21), 0); // remove noise
+    // if(previous.empty()) grey_frame.copyTo(previous);
+    // absdiff(previous, grey_frame, difference);
+    // threshold(difference, difference, threshold_value, 255, THRESH_BINARY);
 
     //hitBuffer logic
     if(hitBuffer >= 10) hitBuffer = 0;
-    if(hitBuffer > 0) hitBuffer++;   
+    if(hitBuffer > 0) hitBuffer++;
 
     //detect ball hit
     int down = 0;
     int right = 0;
     if(hitBuffer == 0){
-//	cout << "HIT CENTER" << endl;
-	for(int i = -1*radius; i < radius; ++i){
-	    for(int j = -1*radius; j < radius; ++j){
-		if(difference.at<uchar>(Point(ball.GetPosition().x+i,ball.GetPosition().y+j)) > threshold_value){
-		    if(i < 0 && j > 0){ //q3++;
-			down--;
-			right++;
-		    }
-		    if(i < 0 && j < 0){ //q2++;
-			down++;
-			right++;
-		    }
-		    if(i > 0 && j < 0){ //q1++;
-			down++;
-			right--;
-		    }
-		    if(i > 0 && j > 0){ //q4++;
-			down--;
-			right--;
-		    }
-		    power++;
-		}
-	    }
-	}
+      //	cout << "HIT CENTER" << endl;
+  	  for(int i = -1*radius; i < radius; ++i){
+  	    for(int j = -1*radius; j < radius; ++j){
+  		    if(fgmask.at<uchar>(Point(ball.GetPosition().x+i,ball.GetPosition().y+j)) > threshold_value){
+  		      if(i < 0 && j > 0){ //q3++;
+        			down--;
+        			right++;
+    		    }
+  		      if(i < 0 && j < 0){ //q2++;
+        			down++;
+        			right++;
+    		    }
+    		    if(i > 0 && j < 0){ //q1++;
+        			down++;
+        			right--;
+    		    }
+    		    if(i > 0 && j > 0){ //q4++;
+        			down--;
+        			right--;
+    		    }
+    		    power++;
+  		    }
+  	    }
+  	  }
 
-	if(power > 100 && hitBuffer == 0 && ball.GetOutOfBounds() == false){
-            ball.SetVelocity(right/20, down/20);
-            hitBuffer++;
-	}
-	power = 0;
-    }    
- 
+    	if(power > 100 && hitBuffer == 0 && ball.GetOutOfBounds() == false){
+        ball.SetVelocity(right/20, down/20);
+        hitBuffer++;
+    	}
+  	  power = 0;
+    }
+
     ball.Update();
     int goal_status = ballInGoal(ball, width);
     if(goal_status!=0){
@@ -113,22 +112,20 @@ int main(int argc, char** argv) {
         ball.Reset(0);
       }
     }
-    
-    ball.Draw(&cameraFrame);
 
-    drawText(cameraFrame, 0, 50, appendNumToText("SCORE: ", left_score));
-    drawText(cameraFrame, width-300, 50, appendNumToText("SCORE: ", right_score));
+    ball.Draw(&frame);
+    drawText(frame, 0, 50, appendNumToText("SCORE: ", left_score));
+    drawText(frame, width-300, 50, appendNumToText("SCORE: ", right_score));
 
-//    difference.copyTo(cameraFrame);
-   
+    // difference.copyTo(cameraFrame);
+
     grey_frame.copyTo(previous);
 
-    imshow("Pong", cameraFrame);
-//    imshow("Pong", ball_mask);
-//    imshow("Pong", fgmask);
-//    imshow("Pong", difference);
+    imshow("Pong", frame);
+    // imshow("Pong", ball_mask);
+    // imshow("fgmask", fgmask);
+    // imshow("Pong", difference);
     if(waitKey(30) >= 0) break;
   }
   return 0;
 }
-
